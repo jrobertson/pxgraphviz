224c31a0d60b541bc29db096417c36f641694298
James Robertson <digital.robertson@gmail.com>
2022-02-23 19:34:36 +0000

    bug fix: Replaced  RXFHelper with RXFReader.

--- lib/pxgraphviz.rb
+++ lib/pxgraphviz.rb
@@ -5,6 +5,7 @@
 
 require 'polyrex'
 require 'graphvizml'
+require 'rxfreader'
 
 
 module RegGem
@@ -17,7 +18,7 @@ hkey_gems
       require pxgraphviz
       class PxGraphViz
       media_type svg
-'      
+'
   end
 end
 
@@ -27,65 +28,65 @@ class PxGraphViz < GraphVizML
 
   attr_reader :doc, :px
 
-  def initialize(obj=nil, style: nil, debug: false, fill: '#778833', 
+  def initialize(obj=nil, style: nil, debug: false, fill: '#778833',
                  stroke: '#999999', text_color: '#ffeecc')
 
     if obj then
 
-      s = RXFHelper.read(obj).first
+      s = RXFReader.read(obj).first
       @doc = import_string(s)
 
       puts 'pxgraphviz: before super'.info if debug
 
-      super(@doc, debug: debug, fill: fill, 
+      super(@doc, debug: debug, fill: fill,
                   stroke: stroke, text_color: text_color)
-      
+
     end
-    
+
     @css = "
       .node polygon {stroke: #{stroke}; fill: #{fill}}
       .node text {fill: #{text_color}}
       .edge path {stroke: #{stroke}}
       .edge polygon {stroke: #{stroke}; fill: #{stroke}}
     "
-    
+
   end
 
   def import(s)
-    s2 = RXFHelper.read(s).first
+    s2 = RXFReader.read(s).first
     @doc = import_string s2
     @g = build_from_nodes Domle.new(@doc.root.xml)
     self
-  end  
-    
-  
+  end
+
+
   protected
-  
+
   def default_stylesheet()
 
 <<STYLE
-  node { 
-    color: #ddaa66; 
+  node {
+    color: #ddaa66;
     fillcolor: #{@fill};
-    fontcolor: #{@text_color}; 
+    fontcolor: #{@text_color};
     fontname: 'Trebuchet MS';
-    fontsize: 8; 
+    fontsize: 8;
     margin: 0.0;
-    penwidth: 1; 
+    penwidth: 1;
     style: filled;
-    shape: #{@shape}; 
+    shape: #{@shape};
   }
-  
+
   a node {
-    color: #0011ee;   
+    color: #0011ee;
   }
 
   edge {
     arrowsize: 0.5;
-    color: #{@stroke}; 
-    fontcolor: #444444; 
-    fontname: Verdana; 
-    fontsize: 8; 
+    color: #{@stroke};
+    fontcolor: #444444;
+    fontname: Verdana;
+    fontsize: 8;
     #{@type}
     weight: 1;
   }
@@ -93,65 +94,65 @@ STYLE
 
   end
 
-  
+
   private
-  
+
   def import_string(s)
 
     @px = if s =~ /<\?pxgraphviz\b/ then
-      
+
 header = "<?polyrex schema='items[type, shape]/item[label, connection]' delimiter =' # '?>
 type: digraph
-shape: box    
+shape: box
 "
       s2 = s.slice!(/<\?pxgraphviz\b[^>]*\?>/)
 
       if s2 then
-        
+
         attributes = %w(id fill stroke text_color).inject({}) do |r, keyword|
           found = s2[/(?<=#{keyword}=['"])[^'"]+/]
           found ? r.merge(keyword.to_sym => found) : r
         end
-        
+
       fill, stroke, text_color = %i(fill stroke text_color).map do |x|
         attributes[x] ? attributes[x] : method(x).call
       end
-      
+
       @css = "
         .node ellipse {stroke: #{stroke}; fill: #{fill}}
         .node text {fill: #{text_color}}
         .edge path {stroke: #{stroke}}
         .edge polygon {stroke: #{stroke}; fill: #{stroke}}
-      "      
-        
-      end 
+      "
+
+      end
       s3 = header + s
       puts 's3: ' + s3.inspect if @debug
       Polyrex.new.import(s3)
-      
+
     else
-      
+
       Polyrex.new(s)
-      
-    end    
+
+    end
 
     @type = @px.summary[:type] == 'digraph' ? 'dir: forward;' : 'dir: none;'
     @shape = @px.summary[:shape] || 'ellipse;'
-    
+
     @style ||= default_stylesheet()
     doc = Rexslt.new(xslt_stylesheet(), @px.to_xml)\
         .to_doc.root.element('nodes')
-    
+
     doc.root.elements.first.insert_before Rexle::Element.new('style')\
         .add_text @style
-    
+
     doc
-    
+
   end
-  
+
   def xslt_stylesheet()
-    
-<<XSLT    
+
+<<XSLT
 <?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
 <xsl:output method="xml" indent="yes" />
@@ -185,12 +186,12 @@ shape: box
         <xsl:element name="a">
           <xsl:attribute name="href">
             <xsl:value-of select="summary/url"/>
-          </xsl:attribute>   
+          </xsl:attribute>
           <xsl:call-template name='node'/>
         </xsl:element>
 
       </xsl:when>
-      <xsl:otherwise> 
+      <xsl:otherwise>
 
         <xsl:call-template name='node'/>
 
@@ -207,33 +208,33 @@ shape: box
 
   <xsl:template name='node'>
      <xsl:element name="node">
-    
+
       <xsl:attribute name="shape">
         <xsl:value-of select="summary/shape"/>
       </xsl:attribute>
-      
+
       <xsl:if test="summary/id">
         <xsl:attribute name="id">
           <xsl:value-of select="summary/id"/>
         </xsl:attribute>
       </xsl:if>
-    
+
       <xsl:if test="summary/class">
         <xsl:attribute name="class">
           <xsl:value-of select="summary/class"/>
         </xsl:attribute>
       </xsl:if>
-      
+
       <xsl:if test="summary/connection">
         <xsl:attribute name="connection">
           <xsl:value-of select="summary/connection"/>
         </xsl:attribute>
-      </xsl:if>    
-    
+      </xsl:if>
+
       <xsl:apply-templates select='summary'/>
       <xsl:apply-templates select='records'/>
 
-    </xsl:element>  
+    </xsl:element>
   </xsl:template>
 
 </xsl:stylesheet>
--- pxgraphviz.gemspec
+++ pxgraphviz.gemspec
@@ -1,15 +1,15 @@
 Gem::Specification.new do |s|
   s.name = 'pxgraphviz'
-  s.version = '0.5.3'
+  s.version = '0.6.0'
   s.summary = 'Generates a GraphViz Markup Language file from a ' + 
       'Polyrex document'
   s.authors = ['James Robertson']
   s.files = Dir['lib/pxgraphviz.rb']
-  s.add_runtime_dependency('polyrex', '~> 1.2', '>=1.2.1')
-  s.add_runtime_dependency('graphvizml', '~> 0.6', '>=0.6.3')
+  s.add_runtime_dependency('polyrex', '~> 1.4', '>=1.4.0')
+  s.add_runtime_dependency('graphvizml', '~> 0.7', '>=0.7.1')
   s.signing_key = '../privatekeys/pxgraphviz.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/pxgraphviz'
 end
